566c2dfde7662e5f5243562f90fc3f99419611b9
Vinukonda,Sai Manikanta Durga Prasad <s533980@ch140016m.nwmissouri.edu>
2019-03-14 18:19:23 -0500

    ViewContoller Updated

---
+++ The Northwest Programming Contest/StudentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StudentsViewController.swift
+//  The Northwest Programming Contest
+//
+//  Created by Vinukonda,Sai Manikanta Durga Prasad on 3/14/19.
+//  Copyright © 2019 Vinukonda,Sai Manikanta Durga Prasad. All rights reserved.
+//
+
+import UIKit
+
+class StudentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
