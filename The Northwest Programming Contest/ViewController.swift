93e277ca30382291c74919fc108d987b5b3aaf21
Vinukonda,Sai Manikanta Durga Prasad <s533980@ch140016m.nwmissouri.edu>
2019-03-14 16:47:24 -0500

    Initial Commit

---
+++ The Northwest Programming Contest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Northwest Programming Contest
+//
+//  Created by Vinukonda,Sai Manikanta Durga Prasad on 3/14/19.
+//  Copyright © 2019 Vinukonda,Sai Manikanta Durga Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
